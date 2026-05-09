26d2d6973dd054de209650302df0323f5d201511
bulyabatracy <tracybulyaba@gmail.com>
2026-05-09 08:45:41 +0300

    webpage

--- scripts/place.js
+++ scripts/place.js
@@ -1,31 +1,31 @@
-// Current Year
+// FOOTER YEAR
 
 document.getElementById("currentyear").textContent =
     new Date().getFullYear();
 
-// Last Modified Date
+// LAST MODIFIED
 
 document.getElementById("lastModified").textContent =
-    `Last Modification: ${document.lastModified}`;
+    `Last Modified: ${document.lastModified}`;
 
-// Static Weather Values
+// WEATHER VALUES
 
 const temperature = 2;
 const windSpeed = 15;
 
-// Wind Chill Function
+// WIND CHILL FUNCTION
 
 function calculateWindChill(temp, speed) {
 
     return (
         13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
+        (0.6215 * temp) -
+        (11.37 * Math.pow(speed, 0.16)) +
+        (0.3965 * temp * Math.pow(speed, 0.16))
     ).toFixed(1);
 }
 
-// Display Wind Chill
+// DISPLAY WIND CHILL
 
 let windChill = "N/A";
 
@@ -37,3 +37,17 @@ if (temperature <= 10 && windSpeed > 4.8) {
 
 document.getElementById("windchill").textContent =
     windChill;
+
+// HAMBURGER MENU
+
+const menuButton = document.querySelector("#menu");
+
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+
+    menuButton.classList.toggle("open");
+
+});
